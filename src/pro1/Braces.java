0383e8452a2fb2ea88564704bc6b0ecba3284cec
lixuehan <xuehanlee@163.com>
2019-06-10 23:43:54 +0800

    add braces jduge

---
+++ src/pro1/Braces.java
@@ -0,0 +1,43 @@
+package pro1;
+
+import java.util.Stack;
+
+/**
+ * Developed by Lee Happily.
+ */
+public class Braces {
+    public static void main(String[] args) {
+        String testCase = "<<><><<>>>";
+        Braces braces = new Braces();
+        boolean result = braces.isPairs(testCase);
+        System.out.println(result);
+
+    }
+
+    public boolean isPairs(String braces) {
+        if (braces == null) {
+            return false;
+        }
+        if (braces.equals("")) {
+            return true;
+        }
+        int index = 0;
+        Stack<Character> stack = new Stack<>();
+
+        while (index < braces.length()) {
+            char c = braces.charAt(index);
+            if (c == '<') {
+                stack.push(c);
+            } else if (c == '>') {
+                if (stack.isEmpty() || stack.peek() != '<') {
+                    return false;
+                }
+
+                stack.pop();
+            }
+            index++;
+        }
+
+        return stack.isEmpty();
+    }
+}
--- src/pro1/Solution.java
+++ src/pro1/Solution.java
@@ -7,6 +7,8 @@ import java.util.Stack;
  * @author leexuehan on 2019/6/10.
  */
 public class Solution {
+    private final static int INVALID = -1;
+    private final static int VALID = 0;
 
     public boolean isValid(String code) {
         if (code == null) {
@@ -18,20 +20,111 @@ public class Solution {
 
         if (!code.startsWith("<") || !code.endsWith(">")) {
             return false;
-        }
-
-        Stack<String> tags = new Stack<>();
-        int codeLen = code.length();
-        int curIndex = 0;
-        while (curIndex < codeLen) {
-            int nextIndex = extractAndUpdateTags(tags, code, curIndex);
-            if (nextIndex == -1) {
-                return false;
-            } else {
-                curIndex = nextIndex;
+        } else {
+            String startTag = extractStartTag(code);
+            String closeTag = extractCloseTag(code);
+            if (tagsValid(startTag) && tagsValid(closeTag)) {
+                String trimed = trimTag(code);
+                if (!isValidContent(trimed)) {
+                    return false;
+                }
             }
         }
-        return tags.isEmpty();
+
+        return true;
+    }
+
+
+    private String trimTag(String code) {
+        int start = code.indexOf('>');
+        int end = code.lastIndexOf('<');
+        return code.substring(start + 1, end);
+    }
+
+    private String extractCloseTag(String code) {
+        int start = code.lastIndexOf('<');
+        int end = code.lastIndexOf('>');
+        return code.substring(start + 2, end);
+    }
+
+    private String extractStartTag(String code) {
+        int close = code.indexOf('>');
+        return code.substring(1, close);
+    }
+
+    private boolean isValidContent(String trimed) {
+        int tagStartIndex = trimed.indexOf('<');
+        int tagCloseIndex = trimed.lastIndexOf('>');
+        if (tagStartIndex != -1 || tagCloseIndex != -1) {
+            if (trimTag(trimed).equals("")) {
+                if (!isTagValid(trimed, tagStartIndex, tagCloseIndex)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            String subs = trimed.substring(tagStartIndex, tagCloseIndex + 1);
+            if (!isValidContent(subs)) {
+                return false;
+            } else {
+                if (!isTagValid(trimed, tagStartIndex, tagCloseIndex)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isTagValid(String code, int fromIndex, int endIndex) {
+        Stack<String> stack = new Stack<>();
+        StringBuilder tmp = new StringBuilder();
+        boolean openTagStart = false;
+        boolean closeTagStart = false;
+        while (fromIndex < endIndex) {
+            char curChar = code.charAt(fromIndex);
+            if (curChar == '<') {
+                openTagStart = true;
+            } else if (curChar == '>') {
+                if (!openTagStart) {
+                    return false;
+                }
+                String cmp;
+                //if this is a closure tag
+                if (tmp.toString().startsWith("/")) {
+                    cmp = tmp.substring(1);
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    if (!stack.peek().equals(cmp)) {
+                        return false;
+                    }
+                }
+                //if this is an open tag
+                else if (!tmp.toString().equals("")) {
+                    cmp = tmp.toString();
+                    stack.push(cmp);
+                    tmp = new StringBuilder();
+                }
+            } else {
+                tmp.append(curChar);
+            }
+            fromIndex++;
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean containsCdata(String content) {
+        return false;
+
+    }
+
+    private boolean containsTags(String content) {
+        return content.indexOf('<') != -1 || content.indexOf('>') != -1;
+    }
+
+    private String extractContentBetweenTags(String code, int fromIndex, int toIndex) {
+
+        return code.substring(fromIndex, toIndex);
     }
 
     private int extractAndUpdateTags(Stack<String> tags, String code, int fromIndex) {
@@ -50,11 +143,29 @@ public class Solution {
         }
         String trimTags = tag.toString().trim();
         System.out.println(trimTags);
-        if (updateTags(tags, trimTags) != -1) {
+        if (!tagsValid(trimTags)) {
+            return INVALID;
+        }
+        if (updateTags(tags, trimTags) != INVALID) {
             return fromIndex + 1;
         } else {
-            return -1;
+            return INVALID;
         }
+    }
+
+    private boolean tagsValid(String trimTags) {
+        if (trimTags.length() == 0 || trimTags.length() > 9) {
+            return false;
+        }
+        int index = 0;
+        while (index < trimTags.length()) {
+            char c = trimTags.charAt(index);
+            if (c < 'A' || c > 'Z') {
+                return false;
+            }
+            index++;
+        }
+        return true;
     }
 
     private int updateTags(Stack<String> tags, String tag) {
@@ -62,32 +173,35 @@ public class Solution {
         if (tag.startsWith("/")) {
             String substring = tag.substring(1);
             if (Objects.equals(substring, "")) {
-                return -1;
+                return INVALID;
             }
             tagCmp = substring;
         }
         if (tags.isEmpty() || !tags.peek().equals(tagCmp)) {
             System.out.println("push tag:" + tag);
             tags.push(tag);
-            return 0;
+            return VALID;
         }
 
         if (tags.peek().equals(tagCmp)) {
             System.out.println("pop tag:" + tag);
             tags.pop();
-            return 0;
+            return VALID;
         }
-        return 0;
+        return VALID;
     }
 
     public static void main(String[] args) {
         String testcase1 = "<A><B></A></B>";
         String testcase2 = "<A><B></B></A>";
+        String testcase3 = "<A>hello<B></B>world</A>";
         Solution solution = new Solution();
-        boolean valid1 = solution.isValid(testcase1);
-        boolean valid2 = solution.isValid(testcase2);
-        System.out.println("isvalid:[" + valid1 + "]");
-        System.out.println("isvalid:[" + valid2 + "]");
+//        boolean valid1 = solution.isValid(testcase1);
+//        boolean valid2 = solution.isValid(testcase2);
+        boolean valid3 = solution.isValid(testcase3);
+//        System.out.println("isvalid:[" + valid1 + "]");
+//        System.out.println("isvalid:[" + valid2 + "]");
+        System.out.println("isvalid:[" + valid3 + "]");
     }
 
 
