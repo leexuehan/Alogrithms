05f2a0d513024988aeb9bede529394d0a93ccc6c
leexuehan <xuehanlee@163.com>
2019-06-11 23:03:40 +0800

    pro2

---
+++ src/main/java/pro2/Solution.java
@@ -0,0 +1,10 @@
+package pro2;
+
+/**
+ * 思路:
+ */
+public class Solution {
+    public String nearestPalindromic(String n) {
+        return "";
+    }
+}
---
+++ src/test/java/pro2/SolutionTest.java
@@ -0,0 +1,7 @@
+package pro2;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
