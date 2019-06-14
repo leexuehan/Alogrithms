5ba01a877323d64e8f43d8beb21f3ef5f870c0cd
leexuehan <xuehanlee@163.com>
2019-06-14 23:27:12 +0800

    pro3 passed

--- src/main/java/pro3/Solution.java
+++ src/main/java/pro3/Solution.java
@@ -2,113 +2,99 @@ package pro3;
 
 /**
  * @author leexuehan on 2019/6/13.
- *         思路：
- *         <p>
- *         L 和 R 都在18位之内，而 Long.MAX_VALUE 为19位,所以可以用一个 long 类型的变量存储数字
- *         首先分别求 L 和 R 的平方根 l,r
- *         然后求 l,r 之间的回文数
- *         ---------对其平方，如果是回文数，则结果数累计加1
- *         如何列举回文数?
- *         ---------分位数列举,i.e:
- *         55~196691 可以分为以下五段：
- *         55~99
- *         <p>
- *         101~999
- *         <p>
- *         1001~9999
- *         <p>
- *         10001~99999
- *         <p>
- *         100001~196691
- *         <p>
- *         <p>
- *         <p>
- *         <p>
- *         <p>
- *         <p>
- *         <p>
- *         <p>
- *         首先寻找 L 和 R 之间的完全平方数 M 和其平方根 n，如果M 和 n 同时满足回文数规则，则累计结果数加 1
- *         <p>
- *         如何判定是完全平方数？
- *         <p>
- *         1 + 3 + 5 + …… + (2n - 1) = (1 + (2n -1))* n /2 = n^2
- *         则 n 为平方根， n^2  为平方数
+ * 思路：
+ * <p>
+ * L 和 R 都在18位之内，而 Long.MAX_VALUE 为19位,所以可以用一个 long 类型的变量存储数字
+ * 首先分别求 L 和 R 的平方根 l,r
+ * 然后求 l,r 之间的回文数
+ * ---------对其平方，如果是回文数，则结果数累计加1
+ * 如何列举回文数?
+ * ---------分位数列举,i.e:
+ * 55~196691 可以分为以下五段：
+ * 55~99
+ * <p>
+ * 101~999 10~99 包含最右位的 mirror
+ * <p>
+ * 1001~9999
+ * <p>
+ * 10001~99999
+ * <p>
+ * 100001~196691
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 首先寻找 L 和 R 之间的完全平方数 M 和其平方根 n，如果M 和 n 同时满足回文数规则，则累计结果数加 1
+ * <p>
+ * 如何判定是完全平方数？
+ * <p>
+ * 1 + 3 + 5 + …… + (2n - 1) = (1 + (2n -1))* n /2 = n^2
+ * 则 n 为平方根， n^2  为平方数
  */
 public class Solution {
+
+
     public int superpalindromesInRange(String L, String R) {
         String start = String.valueOf((int) Math.ceil(Math.sqrt(Long.parseLong(L))));
-        String end = String.valueOf((int) Math.ceil(Math.sqrt(Long.parseLong(R))));
-//        System.out.println("lstart:" + start);
-//        System.out.println("rend:" + end);
+        String end = String.valueOf((int) Math.floor(Math.sqrt(Long.parseLong(R))));
 
         int startLen = start.length();
         int endLen = end.length();
         int ret = 0;
-        boolean[] bools = {true, false};
-        for (int i = startLen; i <= (endLen + 1) / 2; i++) {
-//            System.out.println("bit num:" + i);
-            for (boolean isOdd : bools) {
-                int floor = getFloor(start, i, isOdd);
-                int ceil = getCeil(end, i, isOdd);
-//                System.out.println("floor:" + floor + ",ceil:" + ceil);
-                for (int n = floor; n <= ceil; n++) {
-                    String mirror = mirror(String.valueOf(n), isOdd);
-//                    System.out.println(mirror);
-                    int mirrorValue = Integer.parseInt(mirror);
-                    if (mirrorValue <= Integer.parseInt(end) && mirrorValue >= Integer.parseInt(start)) {
-                        Long result = (long) (mirrorValue * mirrorValue);
-                        if (isMirror(result)) {
-                            System.out.println("get num:" + result);
-                            ret += 1;
-                        }
-                    } else {
-                        break;
+
+        for (int len = startLen; len <= endLen; len++) {
+            System.out.println("bits num:" + len);
+            boolean isOdd = (len % 2) != 0;
+            long floor = (len == startLen ? Integer.parseInt(start.substring(0, (len + 1) / 2)) : getFloor((len - 1) / 2));
+            long ceil = (len == endLen ? Integer.parseInt(end.substring(0, (len + 1) / 2)) : getCeil((len - 1) / 2));
+            for (long i = floor; i <= ceil; i++) {
+                String mirror;
+                long mirrorValue;
+                if (isOdd) {
+                    mirror = mirror(String.valueOf(i), false);
+                    mirrorValue = Integer.parseInt(mirror);
+                } else {
+                    mirror = mirror(String.valueOf(i), true);
+                    mirrorValue = Integer.parseInt(mirror);
+                }
+//                System.out.println("mirrorValue: " + mirrorValue);
+                if (mirrorValue <= Integer.parseInt(end) && mirrorValue >= Integer.parseInt(start)) {
+                    Long result = (long) (mirrorValue * mirrorValue);
+//                    System.out.println(result);
+                    if (isMirror(result)) {
+                        System.out.println("get num:" + result);
+                        ret += 1;
                     }
                 }
             }
+
+
         }
 
         return ret;
     }
 
-    private int getCeil(String end, int len, boolean isOdd) {
-        int ceil;
-        String leftStr = end.substring(0, (end.length() + 1) / 2);
-        int lenOfLeft = leftStr.length();
-        if (lenOfLeft == len) {
-            ceil = Integer.parseInt(leftStr);
-        } else {
-            ceil = getMaxNum(len);
-        }
-        return ceil;
-    }
-
-    //获取半边最小数
-    private int getFloor(String start, int len, boolean isOdd) {
-        String leftStr = start.substring(0, (start.length() + 1) / 2);
-        int lenOfLeft = leftStr.length();
-        int floor;
-        if (lenOfLeft == len) {
-            floor = Integer.parseInt(leftStr);
-        } else {
-            floor = getMinNum(len);
-        }
-        return floor;
-    }
-
-    private int getMinNum(int length) {
-        StringBuilder builder = new StringBuilder("1");
-        for (int index = 1; index < length; index++) {
-            builder.append("0");
+    private int getCeil(int nines) {
+        StringBuilder builder = new StringBuilder("9");
+        int index = 0;
+        while (index < nines) {
+            builder.append("9");
+            index++;
         }
         return Integer.parseInt(builder.toString());
     }
 
-    private int getMaxNum(int length) {
-        StringBuilder builder = new StringBuilder();
-        for (int index = 0; index < length; index++) {
-            builder.append("9");
+    //获取半边最小数
+    private int getFloor(int zeros) {
+        StringBuilder builder = new StringBuilder("1");
+        int index = 0;
+        while (index < zeros) {
+            builder.append("0");
+            index++;
         }
         return Integer.parseInt(builder.toString());
     }
@@ -118,9 +104,9 @@ public class Solution {
         return s.equals(new StringBuilder(s).reverse().toString());
     }
 
-    private String mirror(String left, boolean isOdd) {
+    private String mirror(String left, boolean containsBoundary) {
         StringBuilder reverse = new StringBuilder(left).reverse();
-        if (isOdd) {
+        if (!containsBoundary) {
             return left.substring(0, left.length() - 1) + reverse;
         } else {
             return left + reverse;
--- src/test/java/pro3/SolutionTest.java
+++ src/test/java/pro3/SolutionTest.java
@@ -19,6 +19,48 @@ public class SolutionTest {
     public void testCase2() throws Exception {
         Solution solution = new Solution();
         int ret = solution.superpalindromesInRange("4", "15000");
-        assertEquals(4, ret);
+        assertEquals(7, ret);
+    }
+
+    @Test
+    public void testCase3() {
+        Solution solution = new Solution();
+        int ret = solution.superpalindromesInRange("1", "2");
+        assertEquals(1, ret);
+    }
+
+    @Test
+    public void testCase4() {
+        Solution solution = new Solution();
+        int ret = solution.superpalindromesInRange("4", "6");
+        assertEquals(1, ret);
+    }
+
+    @Test
+    public void testCase5() {
+        Solution solution = new Solution();
+        int ret = solution.superpalindromesInRange("6", "6");
+        assertEquals(0, ret);
+    }
+
+    @Test
+    public void testCase6() {
+        Solution solution = new Solution();
+        int ret = solution.superpalindromesInRange("1", "19028");
+        assertEquals(8, ret);
+    }
+
+    @Test
+    public void testCase7() {
+        Solution solution = new Solution();
+        int ret = solution.superpalindromesInRange("398904669", "13479046850");
+        assertEquals(6, ret);
+    }
+
+    @Test
+    public void name() {
+        long num = 104401;
+        long result = num * num;
+        System.out.println(result);
     }
 }
