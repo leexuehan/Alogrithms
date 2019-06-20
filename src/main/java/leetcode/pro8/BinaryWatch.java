b93132fb09be07722c4d92731afdf09c1d0c0c42
leexuehan <xuehanlee@163.com>
2019-06-20 16:22:59 +0800

    pro8 init

---
+++ src/main/java/leetcode/pro8/BinaryWatch.java
@@ -0,0 +1,12 @@
+package leetcode.pro8;
+
+import java.util.List;
+
+/**
+ * @author leexuehan on 2019/6/20.
+ */
+public class BinaryWatch {
+    public List<String> readBinaryWatch(int num) {
+
+    }
+}
---
+++ src/test/java/leetcode/pro8/BinaryWatchTest.java
@@ -0,0 +1,16 @@
+package leetcode.pro8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author leexuehan on 2019/6/20.
+ */
+public class BinaryWatchTest {
+    @Test
+    public void readBinaryWatch() throws Exception {
+
+    }
+
+}
