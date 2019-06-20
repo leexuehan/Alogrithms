8cd2af67c2af2d526a3c1c25d4c47aa8d0df608c
leexuehan <xuehanlee@163.com>
2019-06-20 17:31:26 +0800

    pro8 not solved

--- src/main/java/leetcode/pro8/BinaryWatch.java
+++ src/main/java/leetcode/pro8/BinaryWatch.java
@@ -1,12 +1,45 @@
 package leetcode.pro8;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author leexuehan on 2019/6/20.
  */
 public class BinaryWatch {
+    //表示分钟，从低位到高位的权值分别为：1、2、4、8、16、32
+    private int[] minutes = new int[6];
+    //表示小时，从低位到高位的权值分别为：1、2、4、8
+    private int[] hours = new int[4];
+
     public List<String> readBinaryWatch(int num) {
+        return null;
+    }
+
+    public List<String> getMinutes(int num) {
+        backTrack(0, num, num);
+        return null;
+    }
+
+    private void backTrack(int index, int numLeft, int targetNum) {
+        if (index >= minutes.length || numLeft == targetNum) {
+            //输出结果
+            System.out.println(Arrays.toString(minutes));
+            return;
+        }
+        minutes[index] = 1;
+        numLeft -= 1;
+        backTrack(index + 1, numLeft, targetNum);
+        minutes[index] = 0;
+        numLeft += 1;
+        backTrack(index + 1, numLeft, targetNum);
 
     }
+
+    public static void main(String[] args) {
+        BinaryWatch binaryWatch = new BinaryWatch();
+        binaryWatch.getMinutes(1);
+    }
+
+
 }
