73e37ae3c29d64797898267bbf818bb1e8953ea7
leexuehan <xuehanlee@163.com>
2019-06-21 11:53:53 +0800

    pro10 init

---
+++ src/main/java/leetcode/pro10/ParenthesesSolution.java
@@ -0,0 +1,12 @@
+package leetcode.pro10;
+
+import java.util.List;
+
+/**
+ * @author leexuehan on 2019/6/21.
+ */
+public class ParenthesesSolution {
+    public List<String> generateParenthesis(int n) {
+
+    }
+}
