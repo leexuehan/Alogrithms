796f6f0688fdf602a3a8966453eb944f4f380f01
lixuehan <xuehanlee@163.com>
2019-07-05 23:10:53 +0800

    jzoffer

---
+++ src/main/java/jzoffer/pro27/Solution.java
@@ -0,0 +1,11 @@
+package jzoffer.pro27;
+
+/**
+ * Developed by Lee Happily.
+ */
+public class Solution {
+    public BinaryTreeNode transfer(BinaryTreeNode root) {
+        return null;
+    }
+
+}
